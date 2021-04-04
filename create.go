a24ce964d806e3c84b6e10e6a97f24d985a387ee
marty <m.karkossa@ultraware.nl>
2021-04-04 02:34:49 +0200

    add NextSequence

--- create.go
+++ create.go
@@ -6,6 +6,20 @@ import (
 	"go.etcd.io/bbolt"
 )
 
+func (br Bucket) NextSequence() int {
+	var i uint64
+	err := br.BucketUpdate(func(b *bbolt.Bucket) error {
+		j, err := b.NextSequence()
+		i = j
+		return err
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return int(i)
+}
+
 // Create stores a new object in the bucket.
 // If the key already exists, it returns ErrObjectAlreadyExists
 func (br Bucket) Create(obj Keyer) error {
